b5a98b6f5221018fb9fbd8bd1b10ad58bdf40d3e
Akstiner96 <andrew.stiner@yahoo.com>
2020-11-19 17:04:02 -0600

    made a modification to the code to allow more than 5 symbols to be called without getting rate limited.

--- Functions/data_pull.py
+++ Functions/data_pull.py
@@ -6,6 +6,7 @@ import os
 from dotenv import load_dotenv
 import alpaca_trade_api as tradeapi
 import requests
+import time
 
 def load():
     #load dotenv in to call api keys with
@@ -15,24 +16,40 @@ def load():
     warnings.warn('Make sure your api keys are renamed to fit this function or alter the function to fit your names')
     alpaca_public = os.getenv('ALPACA_API_KEY')
     alpaca_secret = os.getenv('ALPACA_SECRET_KEY')
-    api = tradeapi.REST(alpaca_public, alpaca_secret, api_version='v2')
+    api = tradeapi.REST(alpaca_public, alpaca_secret, base_url='https://paper-api.alpaca.markets')
 
     return api
     
 
 def data_pull(api):
 
-    #initialize stock choice
     assets = api.list_assets()
+    #Empty list to append the output of the api call to
     tick_list = []
-    tickers = ['SPY']
+    #Update the following list with the stock ticker symbols of your choice
+    tickers = ['SPY','AAPL','GOLD','CI','CVS','TSLA','D','WMT','V','FB','NFLX']
+
+    #Initialize a counter to bypass AlphaVantage's 5 API calls/min rule
+    i = 0
     #Try to call the historical quote of the stock
     for ticker in tickers:
         try:
-            #print(ticker)
             df = api.alpha_vantage.historic_quotes(ticker, adjusted=True, output_format='pandas')
-            df.columns = [f"{ticker} Open", f"{ticker} High", f"{ticker} Low", f"{ticker} Close", f"{ticker} Adjusted Close", f"{ticker} Volume", f"{ticker} Dividend Amount", f"{ticker} Split Coefficient"]
+
+            df.columns = [f"{ticker} Open", 
+            f"{ticker} High", 
+            f"{ticker} Low",
+            f"{ticker} Close",
+            f"{ticker} Adjusted Close",
+            f"{ticker} Volume",
+            f"{ticker} Dividend Amount",
+            f"{ticker} Split Coefficient"]
+
             tick_list.append(df)
+
+            i += 1
+            if i % 5 == 0:
+                time.sleep(60)
         except:
             if ticker in assets:
                 raise Exception('The ticker name is right, but AlphaVantage cannot pull its data, make sure the asset is tradeable')
@@ -52,8 +69,6 @@ def data_clean(data):
     data.drop([col for col in data.columns if 'High' in col], axis=1, inplace=True)
     data.drop([col for col in data.columns if 'Low' in col], axis=1, inplace=True)
     
-    #rename the columns something that is actually understandable
-    #data.columns = ['Ford Open','Ford Close','Ford Volume','Apple Open','Apple Close','Apple Volume']
     return data
 
 api = load()
--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Stock Data Pull Using Alpaca's API and AlphaVantage
 
-This is a reusable dotpy file that will import, clean, and present in DataFrame format, any stock data available from the Alpaca API.
+### **What it does**
 
-To use this, clone the repo, create a dotenv file with your Alpaca keys inside the same folder, and inside the "data_pull" function, replace what is in "tickers" with the symbol of the stock or stocks you would like to view.
+This is a reusable dotpy file that will import, clean, and present in DataFrame format, any stock data available from the Alpaca/AlphaVantage API.
+
+### **Usage Notes**  
+
+* User must have their own Alpaca API keys stored in a dotenv file in the same folder as the data_pull.py file.
+
+
+* The [endpoint URL](https://paper-api.alpaca.markets) for key authentication is set to Alpaca's paper trading domain. Use of live trading will likely require an update to the 'base_url' parameter that is inside the 'load' function where api is defined.
+
+
+* AlphaVantage is used because it provides more data categories than base Alpaca, however, its API call rate limit is far less than Alpaca. For this reason there is a sleep timer added to the 'data_pull' function that adds a minute of wait time after every 5 calls. Limit the 'tickers' list to 5 stocks to get an immediate output. 
+
+
+* The 'data_clean' function is completely personal preference. Comment out lines or add to it to personalize the data output.  
+
+* Alpaca only carries [data]('https://alpaca.markets/docs/api-documentation/api-v2/market-data/') of commodities traded on select exchanges. If an ETF or security is not available through Alpaca, the error '_That is not a proper ticker symbol_' will be raised. 
